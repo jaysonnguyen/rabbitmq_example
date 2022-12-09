6acfe0ef931b3da41ab48a5e5cc7ff75c92133a0
jaysonnguyen <quinv.job@gmail.com>
2022-12-09 15:58:46 +0700

    update

--- receive.py
+++ receive.py
@@ -1,18 +1,16 @@
 import pika, sys, os, time
 
 def main():
-    # credentials = pika.PlainCredentials('quinv', 'quinv')
-    # parameters = pika.ConnectionParameters(
-    #     '34.125.237.190',
-    #     5672,
-    #     '/',
-    #     credentials
-    # )
-    parameters = pika.URLParameters('amqp://quinv:quinv@localhost:5672/%2F')
-    connection = pika.BlockingConnection(parameters)
-    # connection = pika.SelectConnection(parameters=parameters)
+    credentials = pika.PlainCredentials('quinv', 'quinv')
+    parameters = pika.ConnectionParameters(
+        '34.125.237.190',
+        5672,
+        'test',
+        credentials
+    )
 
-    # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(parameters)
+
     channel = connection.channel()
 
     channel.queue_declare(queue='hello')
